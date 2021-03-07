948c535e6ce835cdcf83f16083f3396c61a19a29
e4k9 <eh26steve@gmail.com>
2021-03-07 15:57:41 -0800

    connection for mysql and localhost

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+
+
+app.listen(PORT, function() {
+    console.log(`App now listening at localhost: ${PORT}`)
+});
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Test Mac', false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('One trick pony', false);
