b50ecb55e475969fcea1b0404bdfb73a1c175ee1
e4k9 <eh26steve@gmail.com>
2021-04-04 21:07:11 -0700

    update to server js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 4000;
 
 const app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public/'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
